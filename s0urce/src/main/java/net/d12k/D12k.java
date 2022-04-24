e4b082d7f441bf4a09175315d5bc4e92af0e661b
d12kdev <d12k@outlook.cz>
2022-04-24 20:19:43 +0200

    FabricMC 1

---
+++ s0urce/src/main/java/net/d12k/D12k.java
@@ -0,0 +1,10 @@
+package net.d12k;
+
+import net.fabricmc.api.ModInitializer;
+
+public class D12k implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ s0urce/src/main/java/net/d12k/client/D12kClient.java
@@ -0,0 +1,13 @@
+package net.d12k.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class D12kClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
