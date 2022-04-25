be16cea87515a2c832ec637dd14f317f63545a0d
d12kdev <d12k@outlook.cz>
2022-04-25 19:14:30 +0200

    1.16.5

---
+++ 1.16.5/src/main/java/d12k/tgc/Tgc.java
@@ -0,0 +1,17 @@
+package d12k.tgc;
+
+import net.fabricmc.api.ModInitializer;
+
+public enum Tgc {
+    instance;
+    public String mod_name = "TigerHackClient";
+    public String mod_version = "1.0.0";
+
+    public void load_client(){
+
+    }
+
+    public void stop_client(){
+
+    }
+}
