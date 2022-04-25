6e4803eec3013e2d6654fc91cb1112fb2a8b4d5a
d12kdev <d12k@outlook.cz>
2022-04-25 15:48:33 +0200

    Project Created

---
+++ Client/src/main/java/d12k/tgc/Tgc.java
@@ -0,0 +1,10 @@
+package d12k.tgc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tgc implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ Client/src/main/java/d12k/tgc/client/TgcClient.java
@@ -0,0 +1,13 @@
+package d12k.tgc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TgcClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
